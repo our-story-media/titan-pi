13f62e802567e2a93487db6371154907040a1927
Tom Bartindale <tom.bartindale@monash.edu>
2019-12-05 12:10:20 +0800

    citest

--- rpi/build.sh
+++ rpi/build.sh
@@ -70,7 +70,7 @@ cp $(ls -Art ./pi-gen/deploy/*.zip | tail -n 1) ./sdcard/$FILENAME
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 export PATH=~/bin:$PATH
 
-aws s3 cp ./sdcard/$FILENAME s3://ourstory-v2-live/titan/
+aws s3 cp ./pi-gen/deploy/deploy.zip s3://ourstory-v2-live/titan/
 aws s3 cp s3://ourstory-v2-live/titan/$FILENAME s3://ourstory-v2-live/titan/indaba-rpi.zip
 
 # sleep 1
