1c5794c4d996d66c50f24a3b9a7143bb2c85a04d
Tom Bartindale <tom.bartindale@monash.edu>
2021-10-20 11:11:49 +1100

    ci update

--- supervisor/build.sh
+++ supervisor/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-DOCKER_BUILDKIT=1
+# DOCKER_BUILDKIT=1
 
 echo "Building Indaba Supervisor ARM7"
+
 # ,linux/amd64,linux/arm64, linux/arm/v7
-docker buildx build --platform linux/arm/v7 -t bootlegger/indaba-supervisor .
+docker build --platform linux/arm/v7 -t bootlegger/indaba-supervisor --load .
 # docker buildx build --platform=linux/arm/v7 -t bootlegger/indaba-supervisor --load .
 
+# docker run --mount type=bind,source="$(pwd)"/src,target=/build/src --name indaba-supervisor bootlegger/indaba-supervisor
 
-docker create -ti --name indaba-supervisor bootlegger/indaba-supervisor bash
-docker cp indaba-supervisor:/build/indaba-supervisor ./build/indaba-supervisor
-docker rm -f indaba-supervisor
+# docker create --platform linux/arm/v7 -ti --name indaba-supervisor bootlegger/indaba-supervisor bash
+# docker cp indaba-supervisor:/build/indaba-supervisor ./build/indaba-supervisor
+# docker rm -f indaba-supervisor
