d21092c7fc93ae32c55fad02907f5166ee885abc
Tom Bartindale <tom.bartindale@monash.edu>
2021-10-22 11:14:21 +1100

    artefacts and upload script

--- rpi/build.sh
+++ rpi/build.sh
@@ -66,9 +66,3 @@ FILENAME=indaba-rpi-$VERSION.zip
 # touch ./pi-gen/deploy/deploy.zip
 
 cp $(ls -Art ./deploy/*.zip | tail -n 1) ./sdcard/$FILENAME
-
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-export PATH=~/bin:$PATH
-
-aws s3 cp ./sdcard/$FILENAME s3://ourstory-v2-live/titan/
-aws s3 cp s3://ourstory-v2-live/titan/$FILENAME s3://ourstory-v2-live/titan/indaba-rpi.zip
---
+++ rpi/upload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+VERSION=`cat ../VERSION`
+
+FILENAME=indaba-rpi-$VERSION.zip
+
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+
+export PATH=~/bin:$PATH
+
+aws s3 cp ./sdcard/$FILENAME s3://ourstory-v2-live/titan/
+
+aws s3 cp s3://ourstory-v2-live/titan/$FILENAME s3://ourstory-v2-live/titan/indaba-rpi.zip
