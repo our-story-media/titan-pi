e432bd60e80c60562875b8c1490ff6405b4d90bc
Tom Bartindale <tom.bartindale@monash.edu>
2022-03-25 12:09:24 +1100

    upload .version file on CI

--- rpi/upload.sh
+++ rpi/upload.sh
@@ -4,6 +4,8 @@ VERSION=`cat ./VERSION`
 
 FILENAME=indaba-rpi-$VERSION.zip
 
+echo $VERSION > indaba-rpi.version
+
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 
 export PATH=~/bin:$PATH
@@ -11,3 +13,7 @@ export PATH=~/bin:$PATH
 aws s3 cp ./sdcard/$FILENAME s3://ourstory-v2-live/titan/
 
 aws s3 cp s3://ourstory-v2-live/titan/$FILENAME s3://ourstory-v2-live/titan/indaba-rpi.zip
+
+echo "Uploading Version"
+
+aws s3 cp indaba-rpi.version s3://ourstory-v2-live/titan/
